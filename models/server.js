7078efe54567fabc07348ca56cc8751b4ff2d384
Jacob Sánchez <jacob_ico@outlook.com>
2022-04-28 11:57:01 -0500

    add Socket.io and settings
    se añadio la librería socket.io y se configuro para su uso. así como también se modifico el LISTEN de express a server en el archivo  server.js

--- models/server.js
+++ models/server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
+const { createServer } = require("http");
+
 const { dbConnection } = require("../database/config");
+const { socketController } = require("../sockets/Controller");
 
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
+    this.server = createServer(this.app);
+    this.io = require("socket.io")(this.server);
 
     this.paths = {
       auth: "/api/auth",
@@ -25,6 +30,8 @@ class Server {
     this.middlewares();
     //rutas de mi aplicacion
     this.routes();
+    //Sockets
+    this.sockets();
   }
 
   async conectarDB() {
@@ -62,8 +69,13 @@ class Server {
     this.app.use(this.paths.uploads, require("../routes/uploads"));
   }
 
+  sockets() {
+    this.io.on("connection", socketController);
+  }
+
   listen() {
-    this.app.listen(this.port, () => {
+    //Usabamos server en lugar de express ya que express no tiene sockets
+    this.server.listen(this.port, () => {
       console.log("listening on port " + this.port);
     });
   }
