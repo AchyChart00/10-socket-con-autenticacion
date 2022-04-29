7167e062a18ef1bcad9623c5594afde4b3935dd2
Jacob Sánchez <jacob_ico@outlook.com>
2022-04-29 10:21:06 -0500

    Added front-end JWT verification

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,34 @@
-const socket = io();
+const url = window.location.hostname.includes("localhost")
+  ? "http://localhost:8080/api/auth/"
+  : "https://rest-server-jacob.herokuapp.com/api/auth";
+
+let usuario = null;
+let socket = null;
+
+//validar el tocken del localStorage
+const validarJWT = async () => {
+  const token = localStorage.getItem("token") || "";
+
+  if (token.length <= 10) {
+    window.location = "index.html";
+    throw new Error("No hay token en el servidor");
+  }
+
+  const resp = await fetch(url, {
+    headers: { "x-token": token },
+  });
+
+  const { usuario: userDB, token: tokenDB } = await resp.json();
+  localStorage.setItem("token", tokenDB);
+
+  usuario = userDB;
+};
+
+const main = async () => {
+  validarJWT;
+  await validarJWT();
+};
+
+main();
+
+//const socket = io();
